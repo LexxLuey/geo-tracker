70911962c5666273e5635a4ac18dc6a454b8f6bb
lexxluey <biggestluey@gmail.com>
2023-05-25 11:56:19 +0100

    init app

--- App.js
+++ App.js
@@ -1,53 +1,69 @@
-import { Platform } from 'react-native';
-import { Button, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
-import messages from './src/constants/messages';
-import Home from './src/screens/Home';
+import React, { useEffect, useState } from 'react';
+import MapView, { ProviderProps } from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
 
+const App = () => {
+  const [locations, setLocations] = useState([]);
 
+  let region = undefined;
 
-export default function App() {
+  useEffect(() => {
+    const handlePositionUpdate = async (position) => {
+      setLocations((prevLocations) =>
+        prevLocations.concat({ latitude: position.coords.latitude, longitude: position.coords.longitude })
+      );
+    };
+    Geolocation.requestAuthorization();
+    Geolocation.getCurrentPosition(handlePositionUpdate);
+    Geolocation.watchPosition(handlePositionUpdate, error => console.log("Error: ", error));
+    Geolocation.addListenerOnce('didChangeAuthorizationStatus', status => {
+      if (status === 'authorized') {
+        getUserRegion();
+      } else {
+        clearMapCenterRegion();
+      }
+    });
+    return () => {
+      Geolocation.clearWatch();
+    };
+  }, []);
+
+  const getUserRegion = async () => {
+    try {
+      const location = await Geolocation.getCurrentPosition();
+      region = {
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudinalMetric: 'degrees',
+        longitudinalMetric: 'degrees',
+      };
+      const spanX = 0.5;
+      const spanY = 0.5;
+      setSpanX(spanX);
+      setSpanY(spanY);
+    } catch (error) { }
+  };
+
+  const clearMapCenterRegion = async () => {
+    if (!region) return;
+    const { state, actions } = MapView.propOverridesProvider;
+    state.centerCoordinate = undefined;
+    actions.animateToInitialPosition(state);
+    state.onSnapshotReady(actions);
+  };
+
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
-      <Home />
-    </SafeAreaView>
-
+    <MapView
+      provider={new MapboxGL.AccessTokenProvider().getBase64AuthString()}
+      style={{ width: "100%" }}
+      region={{ ...region ?? { latitude: -37.82529, longitude: 145.12690, zoomLevel: 10 } }}
+    >
+      {/* This condition checks whether there are any locations added into locations array */}
+      {locations && (
+        <GeoJSON source={{ type: "FeatureCollection", features: locations }} />
+      )}
+    </MapView>
   );
-}
+};
 
-const style = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 50,
-    paddingHorizontal: 16,
-    backgroundColor: 'silver',
-  },
-  dataCards: {
-    paddingTop: 40,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-  searchInput: {
-    borderWidth: 1,
-    borderColor: '#fff',
-    width: '70%',
-    marginRight: 8,
-    padding: 8
-  },
-  homeSearchParentInputStyle: {
-    borderBottomColor: '#DDDDDD',
-    borderBottomWidth: 0.6,
-    paddingVertical: Platform.OS == 'ios' ? 10 : 0,
-    marginTop: 20,
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  homeSearchInputStyle: {
-    fontSize: 15,
-    width: '90%',
-  },
-});
+export default App;
